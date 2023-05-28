025441209378363e951d12c1ea32b94da1d978db
NhatAnh9 <tonynhatanh911@gmail.com>
2023-05-28 18:11:42 +0700

    add react

---
+++ React/swp391-app/src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/1.png";
+import product3 from "./assets/products/1.png";
+import product4 from "./assets/products/1.png";
+import product5 from "./assets/products/1.png";
+import product6 from "./assets/products/1.png";
+import product7 from "./assets/products/1.png";
+import product8 from "./assets/products/1.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Bird Food",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Bird Food",
+    price: 1999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Bird Food",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Bird Food",
+    price: 228.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Bird Food",
+    price: 19.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Bird Food",
+    price: 68.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Bird Food",
+    price: 120.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Bird Food",
+    price: 40.0,
+    productImage: product8,
+  },
+];
