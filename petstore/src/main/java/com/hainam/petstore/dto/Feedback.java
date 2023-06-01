298ea5f5d7f9963f9c053f50227a59b3019120aa
Vũ Hải Nam <vuhainam272@gmail.com>
2023-06-01 07:19:46 +0700

    add order and feedback api

--- petstore/src/main/java/com/hainam/petstore/dto/Feedback.java
+++ petstore/src/main/java/com/hainam/petstore/dto/Feedback.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.hainam.petstore.dto;
 
-/**
- *
- * @author vuhai
- */
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Feedback {
-    
+    private int feedbackId;
+    private String message;
+    private String author;
 }
