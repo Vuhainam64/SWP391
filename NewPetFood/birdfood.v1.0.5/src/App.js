f4e1a786e8b2601ac8e74bf0a9aaed26bf330ee6
Vũ Hải Nam <117963352+TienHoang0102@users.noreply.github.com>
2023-06-23 10:48:38 +0700

    Add the Upload Product

--- NewPetFood/birdfood.v1.0.5/src/App.js
+++ NewPetFood/birdfood.v1.0.5/src/App.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import { CreateConainer, Header, MainContainer } from "./components";
+import { useStateValue } from "./context/StateProvider";
+import { getAllFoodItems } from "./components/utils/firebaseFunctions";
+import { actionType } from "./context/reducer";
 
 function App() {
+    const [{ foodItems }, dispatch] = useStateValue();
+
+    const fetchData =async () => {
+        await getAllFoodItems().then(data => {
+            dispatch({
+                type : actionType.SET_FOOD_ITEMS,
+                foodItems: data,
+            })
+        })
+    }
+
+    useEffect(() => {
+        fetchData()
+    },[])
+
     return ( 
         <AnimatePresence>
             <div className="w-screen h-auto flex flex-col bg-primary">
